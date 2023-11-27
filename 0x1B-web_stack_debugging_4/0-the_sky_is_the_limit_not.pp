c41799e9ba659af04741d4e4158632406d2c7519
Romaric250 <lonfonyuyromeo@gmail.com>
2023-11-27 18:45:45 +0000

    updated some things

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
-# puppet file
+# This manuscript increases the amount of traffic an Nginx server can handle
 
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
